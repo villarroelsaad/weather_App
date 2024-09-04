810adf26d5d1fd156ff05961c2047177027f27be
Jose Villarroel <163937499+villarroelsaad@users.noreply.github.com>
2024-09-04 14:32:16 -0400

    v-1.0

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import  { useState } from 'react';
+import './index.css';
+import Search from './components/search';
+import Card from './components/card';
+
+function App() {
+  const [weatherData, setWeatherData] = useState(null);
+
+  const handleSearch = (data) => {
+    setWeatherData(data);
+  };
+
+  return (
+    <div>
+      <header className='h-20  flex justify-end'>
+      <nav className='flex font-semibold text-lg items-center '>
+      <a className=' ' href='https://github.com/villarroelsaad/Acortador_links'><img src='./github.svg' className='h-12 transition-all opacity-80 hover:opacity-50' /></a>
+    </nav>
+      </header>
+    <section className="flex flex-col max-w-700 flex-wrap mt-16 border-2 rounded-3xl border-slate-200 justify-center items-center self-center bg-zinc-50 bg-opacity-40 shadow">
+      <h1 className='text-3xl mb-4 font-semibold ' >Weather App</h1>
+   
+      <Search onSearch={handleSearch} />
+   
+      {weatherData && <Card data={weatherData} />}
+      </section>
+   </div>
+  );
+}
+
+export default App;
